4ec1bc810a82bbae8e387abba8f9d587ab2f170e
aghora7 <ukikieran@gmail.com>
2021-06-29 18:11:25 +0530

    Sepolicy: adopt to our changes

--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by select legacy products that
-# inherit from PixelExperience
+# inherit from LESSAOSP
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
